961ffd1df521b709a89228ac7f45b93ae51a7389
Pablo Fontenla Miota <pfontenl@cbr4s3.42barcelona.com>
2024-11-29 12:02:41 +0100

    what the fuck am i doing

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int initialize_listener(const int port)
 		std::cerr << "Bad ini of protocol" << std::endl;//cerr used
 		return (-1);
 	}
-	listener = socket(AF_INET /* IPv4 */, SOCK_STREAM | SOCK_NONBLOCK /* TCP and Non-Blocking */, protocol->p_proto /* TCP Protocol */);
+	listener = socket(AF_INET /* IPv4 */, SOCK_STREAM | SOCK_NONBLOCK /* TCP and Non-Blocking */, protocol->p_proto /* TCP Protocolgit stat */);
 	if (-1 == listener)
 		return (-1);
 
@@ -43,7 +43,52 @@ int cycle(struct pollfd* monitored, const char *const password)
 		pollret = poll(monitored, 2 /*length of monitored (array of fds), var is of type nfds_t*/, 0 /*Timeout in ms, set to 0 so it's non-blocking ("If timeout is zero, then poll() will return without blocking.", Source: https://man.freebsd.org/cgi/man.cgi?poll)*/);
 		if (pollret > 0)
 		{
-			std::cout << "Detected activity on " << pollret << " socket(s)!" << std::endl;
+			for	(int i = 0; i < 2; i++)
+			{
+				struct pollfd current = monitored[i];
+				if (current.events || current.revents)
+				{
+					std::cout << "--------------------------------------------------------------------" << std::endl;
+					// std::cout << "POLLIN: " << POLLIN << std::endl; // 1
+					// std::cout << "POLLRDNORM: " << POLLRDNORM << std::endl; // 64
+					// std::cout << "POLLRDBAND: " << POLLRDBAND << std::endl; // 128
+					// std::cout << "POLLPRI: " << POLLPRI << std::endl; // 2
+					// std::cout << "POLLOUT: " << POLLOUT << std::endl; // 4
+					// std::cout << "POLLWRNORM: " << POLLWRNORM << std::endl; // 256
+					// std::cout << "POLLWRBAND: " << POLLWRBAND << std::endl; // 512
+					// std::cout << "POLLERR: " << POLLERR << std::endl; // 8
+					// std::cout << "POLLHUP: " << POLLHUP << std::endl; // 16
+					// std::cout << "POLLRDHUP: " << POLLRDHUP << std::endl; // 8192
+					// std::cout << "POLLNVAL: " << POLLNVAL << std::endl; // 32
+					std::cout << "Potential activity on monitored[" << i <<"], fd " << current.fd << std::endl;
+					if (current.events)
+					{
+						std::cout << "[EVENTS] (Raw Value: " << current.events << ")" << std::endl;
+						if (current.events & POLLIN)
+							std::cout << "- POLLIN" << std::endl;
+						if (current.events & POLLOUT)
+							std::cout << "- POLLOUT" << std::endl;
+						if (current.events & POLLNVAL)
+							std::cout << "- POLLNVAL" << std::endl;
+					}
+					else
+						std::cout << "No Events"<< std::endl;
+					std::cout << std::endl;
+					if (current.revents)
+					{
+						std::cout << "[REVENTS] (Raw Value: " << current.revents << ")" << std::endl;
+						if (current.revents & POLLIN)
+							std::cout << "- POLLIN" << std::endl;
+						if (current.revents & POLLOUT)
+							std::cout << "- POLLOUT" << std::endl;
+						if (current.revents & POLLNVAL)
+							std::cout << "- POLLNVAL" << std::endl;
+					}
+					else
+						std::cout << "No Revents"<< std::endl;
+					std::cout << "--------------------------------------------------------------------" << std::endl;
+				}
+			}
 			/*
 				identify which fds we want to do something with
 				DO IT
@@ -65,24 +110,22 @@ int main (int argc, char** argv)
 
 	if (argc != 3)
 	{
-		std::cout << "Incorrect amount of arguments. Expected 2, have " << argc-1 << std::endl;
+		std::cout << "Incorrect amount of arguments. Expected 2, have " << argc - 1 << std::endl << "Usage: ./ircserv <port> <password>" << std::endl;
 		return (1);
 	}
-
-
-	listener = initialize_listener(std::atoi(argv[1]));
+	listener = initialize_listener(std::atoi(argv[1])); // potencial error de parsing con los out-of-range y/o cosas que no sean numeros mi rey
 	if (-1 == listener)
 	{
 		//possibly use my pterror from minishell _glopez-m
 		return (1);//good that no malloc was done yet
 	}
-	monitored = static_cast<struct pollfd*>(malloc(2 * sizeof(struct pollfd))); // erm... what the malloc? (-42 social credit) (can / should we use a vector?)
+	monitored = static_cast<struct pollfd*>(malloc(2 * sizeof(struct pollfd))); // erm... what the malloc? (-42 social credit) (can / should we use a vector?) // not even a calloc? damn dude ok // why do we have 2 sockets??
 	if (!monitored)
 	{
 		close(listener);
 		return (ENOMEM);
 	}
-	std::cout << "Sockets ready! Listening..." << std::endl;
+	std::cout << "Sockets ready!" << std::endl;
 	cycle(monitored, argv[2]);
 	return (0);
 }
