3426724b90dcd61ea33efe321835931e597ecf61
Just A Green Toast <mariofon03@gmail.com>
2024-12-10 18:02:43 +0100

    se me ha ido

--- ft_irc.hpp
+++ ft_irc.hpp
@@ -14,4 +14,50 @@
 # define MAX_CONN_QUEUE 128
 
 # define DEBUG 0
+
+class Client
+{
+    public:
+        int fd;
+        std::string rd_buff;
+        std::string wr_buff;
+
+        Client(int fd)
+        {
+            this->fd = fd;
+            rd_buff = "";
+            wr_buff = "";
+        }
+
+        Client &operator=(const Client &other)
+        {
+            fd = other.fd;
+            rd_buff = other.rd_buff;
+            wr_buff = other.wr_buff;
+            return *this;
+        }
+
+        ssize_t Read();
+        ssize_t Send();
+};
+
+class Server
+{
+    private:
+        const char *const password;
+        std::vector<struct pollfd> pollfds;
+        std::vector<Client> clients;
+
+        int initialize_listener(const int port);
+    public:
+        Server(const char *const password) : password(password) { }
+        int init(int port);
+        void AddClient(int fd, short flags);
+        void RemoveClient(size_t index);
+        void AcceptClient();
+        int OnClientRead(size_t index);
+        int OnClientSend(size_t index);
+        int cycle();
+
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <cstdio> // std::perror (no lo meto en el header porque no se si puede usarse o no el perror)
 
 // PORT and PASSWORD ar just placeholders to be replaced with actual variables
-
-int initialize_listener(const int port)
+int Server::initialize_listener(const int port)
 {
 	int listener;
 	struct protoent* protocol = getprotobyname("TCP");
@@ -35,22 +34,140 @@ int initialize_listener(const int port)
 		return (-1);
 	return (listener);	
 }
-int cycle(std::vector<struct pollfd>& monitored, const char *const password)
+
+int Server::init(int port)
+{
+	int listener;
+	listener = initialize_listener(port); // potencial error de parsing con los out-of-range y/o cosas que no sean numeros mi rey
+	if (-1 == listener)
+	{
+		//possibly use my pterror from minishell _glopez-m
+		return (1);//good that no malloc was done yet
+	}
+	AddClient(listener, POLLIN | POLLHUP);
+	return 0;
+}
+
+void Server::AddClient(int fd, short flags)
+{
+	struct pollfd poll_data;
+	poll_data.fd = fd;
+	poll_data.events = flags;
+	poll_data.revents = 0;//0 since this will get filled when poll() gets called
+	pollfds.push_back(poll_data);
+	clients.push_back(Client(fd));
+}
+
+void Server::RemoveClient(size_t index)
+{
+	close(pollfds[index].fd);
+	pollfds.erase(pollfds.begin() + index);
+	clients.erase(clients.begin() + index);
+}
+
+void Server::AcceptClient()
+{
+	// https://reactive.so/post/42-a-comprehensive-guide-to-ft_irc/
+	struct sockaddr_in client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
+	int client_fd = -1;
+	while (client_fd < 0)//this should not be needed
+		client_fd = accept(pollfds[0].fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len); // returns -1 on failure, usually EAGAIN due to non-block//if properly programmed, we never get to see EAGAIN
+	AddClient(client_fd, POLLIN | POLLOUT | POLLHUP); // we might want some global precompiler thing for these, in case its not just POLLIN walways
+	//do we want to do anything else with the newcomer? like putting there recent messages or something
+	std::cout << "WORLD WIDE NOISE 🗣 🗣 🗣" << std::endl;//DEBUG
+}
+
+int Server::OnClientRead(size_t index)
+{
+	Client &client = clients[index];
+	ssize_t bytes_read = client.Read();
+	if (!bytes_read)//consider checking for POLLHUP instead // https://stackoverflow.com/questions/74627334/no-pollhup-event-when-poll-on-tcp-socket-and-remote-closed
+	{
+		std::cout << "Client Disconnected" << std::endl;//DEBUG?
+		RemoveClient(index); // should we i-- after this? might be skipping over a client
+	}
+	else if (bytes_read < 0 && errno != EAGAIN && errno != EWOULDBLOCK) // can we use errno?
+	{
+		std::cerr << "error when reading from [" << index << "]" << std::endl; // std::cerr
+		perror("read" ); // perror
+		RemoveClient(index); // should we i-- after this? might be skipping over a client
+		// close(server_fd);
+		return 1;//don't return, try to handle the error here and "continue;" only return if we are truly fucked
+	}
+	else if (bytes_read > 0)
+	{
+		std::cout << "Received message: " << client.rd_buff << std::endl;
+		for (size_t i = 0; i < clients.size(); i++)
+			if (i != index)
+				clients[i].wr_buff += "Message From Client: " + client.rd_buff + '\n';
+		client.rd_buff = "";
+	}
+	return 0;
+}
+
+int Server::OnClientSend(size_t index)
+{
+	Client &client = clients[index];
+	ssize_t out = client.Send();
+	if (out < 0 && errno != EAGAIN && errno != EWOULDBLOCK) // can we use errno?
+	{
+		std::cerr << "error when sending from [" << index << "]" << std::endl; // std::cerr
+		perror("send"); // perror
+		RemoveClient(index); // should we i-- after this? might be skipping over a client
+		// close(server_fd);
+		return 1;//don't return, try to handle the error here and "continue;" only return if we are truly fucked
+	}
+	return 0;
+}
+
+ssize_t Client::Read()
+{
+	char buffer[1];
+	ssize_t bytes_read = recv(fd, buffer, sizeof(buffer), MSG_DONTWAIT);
+	while (bytes_read > 0 && buffer[bytes_read - 1] != '\n')
+	{
+		for (size_t j = 0; j < sizeof(buffer); j++)
+			rd_buff += buffer[j];
+		// std::cout  << "msg: " << msg << std::endl;
+		bytes_read = recv(fd, buffer, sizeof(buffer), MSG_DONTWAIT); // this can block? (nc -C + Ctrl-D) // is client socket non-blocking?
+	}
+	return bytes_read;
+}
+
+ssize_t Client::Send()
+{
+	char buffer[1];
+	buffer[0] = wr_buff.c_str()[0];
+	ssize_t out = send(fd, buffer, sizeof(buffer), MSG_DONTWAIT);
+	while (out > 0 && wr_buff.length() > 0)
+	{
+		if (wr_buff.length() > 1)
+		{
+			wr_buff = wr_buff.substr(1);
+			buffer[0] = wr_buff.c_str()[0];
+			send(fd, buffer, sizeof(buffer), MSG_DONTWAIT);
+		}
+		else { wr_buff = ""; }
+	}
+	return out;
+}
+
+int Server::cycle()
 {
 	int pollret;
-	int monit_size;
+	size_t monit_size;
 	(void)password;
-	std::string rd_buff[250]; // we don't have a user class / struct yet so the message buffer is here
-	std::string wr_buff[250]; // we don't have a user class / struct yet so the message buffer is here
 	while (1)
 	{
-		monit_size = monitored.size();
-		pollret = poll(monitored.data(), monitored.size() /*length of monitored (array of fds), var is of type nfds_t*/, 0 /*Timeout in ms, set to 0 so it's non-blocking ("If timeout is zero, then poll() will return without blocking.", Source: https://man.freebsd.org/cgi/man.cgi?poll)*/);
+		monit_size = pollfds.size();
+		pollret = poll(pollfds.data(), monit_size, 0 /*Timeout in ms, set to 0 so it's non-blocking ("If timeout is zero, then poll() will return without blocking.", Source: https://man.freebsd.org/cgi/man.cgi?poll)*/);
 		if (pollret > 0)
 		{
-			for	(int i = 0; i < monit_size && pollret; i++) // we're potentially adding / removing clients from the vector, careful about the index and where it ends
+			for	(size_t i = 0; i < monit_size && pollret; i++) // we're potentially adding / removing clients from the vector, careful about the index and where it ends
 			{
-				struct pollfd current = monitored[i];
+				struct pollfd current = pollfds[i];
+				Client &client = clients[i];
 				if (DEBUG)
 					std::cout << "Pollret: " << pollret << " |fd: " << current.fd << " |revents " << current.revents <<std::endl;//DEBUG
 				if (!(current.revents & current.events)) { continue; }
@@ -92,83 +209,23 @@ int cycle(std::vector<struct pollfd>& monitored, const char *const password)
 				if (DEBUG)
 					std::cout << "[REVENTS] (Raw Value: " << current.revents << ")" << std::endl;
 				if (i == 0 && (current.revents & current.events)) //listener will always be [0]
-				{
-					// https://reactive.so/post/42-a-comprehensive-guide-to-ft_irc/
-					struct sockaddr_in client_addr;
-				    socklen_t client_addr_len = sizeof(client_addr);
-					int client_fd = -1;
-					struct pollfd newcomer;
-				    while (client_fd < 0)//this should not be needed
-						client_fd = accept(monitored[0].fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len); // returns -1 on failure, usually EAGAIN due to non-block//if properly programmed, we never get to see EAGAIN
-					newcomer.fd = client_fd;
-					newcomer.events = POLLIN | POLLOUT | POLLHUP /*we might want some global precompiler thing for these, in case its not just POLLIN walways*/;
-					newcomer.revents = 0;
-					monitored.push_back(newcomer);
-					//do we want to do anything else with the newcomer? like putting there recent messages or something
-					std::cout << "WORLD WIDE NOISE 🗣 🗣 🗣" << std::endl;//DEBUG
-				}
+					AcceptClient();
 				else if (current.revents & current.events)
 				{
 					if (current.revents & POLLIN) // might be redundant later on
 					{
 						if (DEBUG)
 							std::cout << "- POLLIN" << std::endl;
-						char buffer[1];
-						ssize_t bytes_read = recv(current.fd, buffer, sizeof(buffer), MSG_DONTWAIT);
-						while (bytes_read > 0 && buffer[bytes_read - 1] != '\n')
-						{
-							for (size_t j = 0; j < sizeof(buffer); j++)
-								rd_buff[i] += buffer[j];
-							// std::cout  << "msg: " << msg << std::endl;
-							bytes_read = recv(current.fd, buffer, sizeof(buffer), MSG_DONTWAIT); // this can block? (nc -C + Ctrl-D) // is client socket non-blocking?
-						}
-						if (!bytes_read)//consider checking for POLLHUP instead // https://stackoverflow.com/questions/74627334/no-pollhup-event-when-poll-on-tcp-socket-and-remote-closed
-						{
-							std::cout << "Client Disconnected" << std::endl;//DEBUG?
-							close(current.fd);
-							monitored.erase(monitored.begin() + i); // should we i-- after this? might be skipping over a client
-						}
-						else if (bytes_read < 0 && errno != EAGAIN && errno != EWOULDBLOCK) // can we use errno?
-						{
-							perror("read"); // perror
-							close(current.fd);
-							monitored.erase(monitored.begin() + i); // should we i-- after this? might be skipping over a client
-							// close(server_fd);
-							return 1;//don't return, try to handle the error here and "continue;" only return if we are truly fucked
-						}
-						else if (bytes_read > 0)
-						{
-							std::cout << "Received message: " << rd_buff[i] << std::endl;
-							for (int j = 0; j < 250; j++)
-								if (j != i)
-									wr_buff[j] = "Message From Client: " + rd_buff[i] + '\n';
-							rd_buff[i] = "";
-						}
+						int out = OnClientRead(i);
+						if (out)
+							return out;
 					}
 					// Write (cutrisimo y asqueroso)
-					if (current.events & POLLOUT && wr_buff[i].length() > 0)
+					if (current.events & POLLOUT && client.wr_buff.length() > 0)
 					{
-						char buffer[1];
-						buffer[0] = wr_buff[i].c_str()[0];
-						ssize_t out = send(current.fd, buffer, sizeof(buffer), MSG_DONTWAIT);
-						while (out > 0 && wr_buff[i].length() > 0)
-						{
-							if (wr_buff[i].length() > 1)
-							{
-								wr_buff[i] = wr_buff[i].substr(1);
-								buffer[0] = wr_buff[i].c_str()[0];
-								send(current.fd, buffer, sizeof(buffer), MSG_DONTWAIT);
-							}
-							else { wr_buff[i] = ""; }
-						}
-						if (out < 0 && errno != EAGAIN && errno != EWOULDBLOCK) // can we use errno?
-						{
-							perror("send"); // perror
-							close(current.fd);
-							monitored.erase(monitored.begin() + i); // should we i-- after this? might be skipping over a client
-							// close(server_fd);
-							return 1;//don't return, try to handle the error here and "continue;" only return if we are truly fucked
-						}
+						int out = OnClientSend(i);
+						if (out)
+							return out;
 					}
 				}
 				if (DEBUG)
@@ -197,25 +254,13 @@ int cycle(std::vector<struct pollfd>& monitored, const char *const password)
 
 int main (int argc, char** argv)
 {
-	int listener;
-	std::vector<struct pollfd> monitored;
-	struct pollfd name_this;
-
 	if (argc != 3)
 	{
 		std::cout << "Incorrect amount of arguments. Expected 2, have " << argc - 1 << std::endl << "Usage: ./ircserv <port> <password>" << std::endl;
 		return (1);
 	}
-	listener = initialize_listener(std::atoi(argv[1])); // potencial error de parsing con los out-of-range y/o cosas que no sean numeros mi rey
-	if (-1 == listener)
-	{
-		//possibly use my pterror from minishell _glopez-m
-		return (1);//good that no malloc was done yet
-	}
-	name_this.fd = listener;
-	name_this.events = POLLIN | POLLHUP;
-	name_this.revents = 0;//0 since this will get filled when poll() gets called
-	monitored.push_back(name_this);
+	Server server(argv[2]);
+	if (server.init(std::atoi(argv[1]))) { return 1; }
 	/*
 	monitored = static_cast<struct pollfd*>(malloc(2 * sizeof(struct pollfd))); // erm... what the malloc? (-42 social credit) (can / should we use a vector?) // not even a calloc? damn dude ok // why do we have 2 sockets??
 	if (!monitored)
@@ -226,6 +271,6 @@ int main (int argc, char** argv)
 	*/
 	std::cout << "Socket ready! Listening on port " << std::atoi(argv[1]) << "..." << std::endl;
 
-	cycle(monitored, argv[2]);
+	server.cycle();
 	return (0);
 }
