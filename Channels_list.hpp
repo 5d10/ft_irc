fca5d5e2ac30ceb80a82bfb51b3c17041fcc3753
Guillem López Muñiz <glopez-m@cbr8s2.42barcelona.com>
2025-01-14 15:51:29 +0100

    Sketched possible data structures

---
+++ Channels_list.hpp
@@ -0,0 +1,25 @@
+//These two are aligned by index
+std::list<Client> clients;
+std::vector<pollfds> fds;//MUST be a vector
+//We can't know for sure each time where in memmory our client will be
+// so searching is needed
+
+std::map<std::string, Client*>;
+Client& find_client(unsigned int i, std::list<Client> clients);
+
+class Channel
+{
+	std::string name;
+	std::vector<std::string> clients;
+	//other stuff
+	//.
+	//.
+	//.
+}
+
+class Channels
+{
+	std::vector<Channel> channels;
+	public:
+		void removeFromAll();
+};
---
+++ Task.hpp
@@ -0,0 +1,8 @@
+class Task
+{
+	public:
+		Task(std::string feed);
+		/*whatever*/ prefix;
+		enum command;
+		std::vector<std::string> params;
+}
